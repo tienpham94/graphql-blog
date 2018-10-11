b379808fe5edc8d8ef818777acb27536878527a5
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-11 09:58:58 +0300

    Mutation component

--- src/Posts/NewPost.js
+++ src/Posts/NewPost.js
@@ -4,62 +4,13 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export default class NewPost extends Component {
-  state = {
-    title: '',
-    body: ''
-  };
-
-  handleInput = e => {
-    const formData = {};
-    formData[e.target.name] = e.target.value;
-    this.setState({ ...formData });
-  };
-
   render() {
-    const { title, body } = this.state;
     return (
       <div>
         <h1>New Post</h1>
-        <Mutation
-          mutation={NEW_POST}
-          variables={{
-            title,
-            body
-          }}
-        >
-          {createPost => (
-            <form
-              onSubmit={e => {
-                e.preventDefault();
-                createPost()
-                  .then(() => {
-                    this.setState({
-                      title: '',
-                      body: ''
-                    });
-                  })
-                  .catch(e => console.log(e));
-              }}
-            >
-              <input
-                name="title"
-                type="text"
-                onChange={this.handleInput}
-                value={title}
-                placeholder="title"
-              />
-              <textarea
-                name="body"
-                type="text"
-                onChange={this.handleInput}
-                value={body}
-                placeholder="body"
-              />
-              <button>Submit</button>
-            </form>
-          )}
+        <Mutation mutation={NEW_POST}>
+          {createPost => <PostForm onSubmit={createPost} />}
         </Mutation>
-        {/* <PostForm /> */}
       </div>
     );
   }
--- src/Posts/PostForm.js
+++ src/Posts/PostForm.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 
 export default class PostForm extends Component {
+  state = {
+    title: '',
+    body: ''
+  };
+
+  handleInput = e => {
+    const formData = {};
+    formData[e.target.name] = e.target.value;
+    this.setState({ ...formData });
+  };
+
   render() {
+    const { onSubmit } = this.props;
+    const { title, body } = this.state;
     return (
-      <form>
-        <input type="text" placeholder="title" />
-        <textarea type="text" placeholder="body" />
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          onSubmit({
+            variables: {
+              title,
+              body
+            }
+          })
+            .then(() => {
+              this.setState({
+                title: '',
+                body: ''
+              });
+            })
+            .catch(e => console.log(e));
+        }}
+      >
+        <input
+          name="title"
+          type="text"
+          onChange={this.handleInput}
+          value={title}
+          placeholder="title"
+        />
+        <textarea
+          name="body"
+          type="text"
+          onChange={this.handleInput}
+          value={body}
+          placeholder="body"
+        />
         <button>Submit</button>
       </form>
     );
