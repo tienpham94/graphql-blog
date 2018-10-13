dc713b5917336d56e0591780c9047c34de712205
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-13 19:34:44 +0300

    Apollo persist cache

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Post from './Posts/Post';
 import Posts from './Posts/Posts';
@@ -8,48 +7,26 @@ import NewPost from './Posts/NewPost';
 import logo from './logo.svg';
 import './App.css';
 
-const defaultState = {
-  isEditMode: false
-};
-
-// Connecting out site to the GraphQL API
-const client = new ApolloClient({
-  uri: 'https://api-uswest.graphcms.com/v1/cjjoov78z2j9t01gclcnhwlb9/master',
-  clientState: {
-    defaults: defaultState,
-    resolvers: {}
-  }
-});
-
-// Running our query outside of React
-// client
-//   .query({
-//     query: testQuery
-//   })
-//   .then(res => console.log(res));
-
 // Apollo Provider attached the client to our React app
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <Router>
-          <div className="App">
-            <header className="App-header">
-              <Link to={'/'}>
-                <h1 className="App-title">GraphQL is Great</h1>
-              </Link>
-            </header>
-            <main>
-              <Switch>
-                <Route exact path="/" component={Posts} />
-                <Route exact path="/post/new" component={NewPost} />
-                <Route path="/post/:id" component={Post} />
-              </Switch>
-            </main>
-          </div>
-        </Router>
-      </ApolloProvider>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to={'/'}>
+              <h1 className="App-title">GraphQL is Great</h1>
+            </Link>
+          </header>
+          <main>
+            <Switch>
+              <Route exact path="/" component={Posts} />
+              <Route exact path="/post/new" component={NewPost} />
+              <Route path="/post/:id" component={Post} />
+            </Switch>
+          </main>
+        </div>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const defaultState = {
+  isEditMode: false
+};
+
+const cache = new InMemoryCache();
+
+persistCache({
+  cache,
+  storage: window.localStorage
+}).then(() => {
+  // Connecting out site to the GraphQL API
+  const client = new ApolloClient({
+    cache,
+    uri: 'https://api-uswest.graphcms.com/v1/cjjoov78z2j9t01gclcnhwlb9/master',
+    clientState: {
+      defaults: defaultState,
+      resolvers: {}
+    }
+  });
+
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+  );
+  registerServiceWorker();
+});
