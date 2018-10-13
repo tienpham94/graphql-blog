509d07f3b149ba5f3218eb7b5e9352982a91bf08
Tien Pham <r3dsky02@yahoo.com.vn>
2018-10-13 19:31:46 +0300

    Optimistic UI

--- src/Posts/Post.js
+++ src/Posts/Post.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import UpdatePost from './UpdatePost';
 import EditMode from './EditMode';
@@ -28,6 +28,45 @@ export default class Post extends Component {
               ) : (
                 <section>
                   <h1>{post.title}</h1>
+                  <Mutation
+                    mutation={UPDATE_POST}
+                    variables={{
+                      id: post.id,
+                      check: !post.check
+                    }}
+                    optimisticResponse={{
+                      __typename: 'Mutation',
+                      updatePost: {
+                        __typename: 'Post',
+                        check: !post.check
+                      }
+                    }}
+                    update={(cache, { data: { updatePost } }) => {
+                      const data = cache.readQuery({
+                        query: POST_QUERY,
+                        variables: {
+                          id: post.id
+                        }
+                      });
+                      data.post.check = updatePost.check;
+                      cache.writeQuery({
+                        query: POST_QUERY,
+                        data: {
+                          ...data,
+                          post: data.post
+                        }
+                      });
+                    }}
+                  >
+                    {updatePost => (
+                      <input
+                        style={{ height: '100px' }}
+                        type="checkbox"
+                        checked={post.check}
+                        onChange={updatePost}
+                      />
+                    )}
+                  </Mutation>
                 </section>
               )}
             </div>
@@ -45,7 +84,16 @@ const POST_QUERY = gql`
       id
       title
       body
+      check
     }
     isEditMode @client
+  }
+`;
+
+const UPDATE_POST = gql`
+  mutation updatePost($check: Boolean, $id: ID!) {
+    updatePost(where: { id: $id }, data: { check: $check }) {
+      check
+    }
   }
 `;
